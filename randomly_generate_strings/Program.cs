bca92773ac3bbe4675bef2578c859a423992ee83
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-03-10 21:45:22 +1100

    Initial commit

---
+++ randomly_generate_strings/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * C# Program to Randomly Generate Strings
+ */
+
+using System;
+using System.IO;
+
+namespace randomly_generate_strings
+{
+    static class Random
+    {
+        public static string GetRandomString()
+        {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", "");
+            return path;
+        }
+    }
+}
